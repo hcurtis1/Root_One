c3db7f7c5ae7231fa1208f819b20a8139f5853f5
Henry Curtis <hcuk106@gmail.com>
2017-03-17 16:35:58 +0000

    Scroll Java added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-angular.module('RootOne', ['ngRoute', 'RouteControllers']);
+angular.module('RootOne', ['ngRoute', 'RouteControllers', 'angularCSS']);
  
 angular.module('RootOne').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  // Enable href routing without hashes
  
     $routeProvider.when('/', {
         templateUrl: 'HTML/Home.html',
-        controller: 'HomeController'
+        controller: 'HomeController',
+        css: 'CSS/Desktop.css'
     })
     .when('/about', {
         templateUrl: 'HTML/About.html',
